83a7a09068bdeb89514d1a003a5f80c71ba1c9d3
Devesh (Dave) <41635993+andrewflik@users.noreply.github.com>
2021-06-20 17:39:13 +0530

    Update wsgi.py

--- fanfair/wsgi.py
+++ fanfair/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fans_clone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fanfair.settings')
 
 application = get_wsgi_application()
