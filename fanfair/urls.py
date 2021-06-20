e5096c11fe0ef5468a2a1df4aba0659b39ec9b9c
andrewflik <deveshrajput978@gmail.com>
2021-06-20 17:41:11 +0530

    changes

--- fanfair/urls.py
+++ fanfair/urls.py
@@ -1,4 +1,4 @@
-"""fans_clone URL Configuration
+"""fanfair URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
